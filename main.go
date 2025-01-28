8a51a715a255bee5f05ff40a6753d77588af7a13
Jonathan <jonathan.moura@easypark.net>
2025-01-28 10:08:39 -0300

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"embed"
+)
+
+func main() {
+}
+
+//go:embed schemas/*.json
+var files embed.FS
+
+func GetSchema(name string) ([]byte, error) {
+	return files.ReadFile("schemas/" + name)
+}
+
+func GetMetadata(name string) ([]byte, error) {
+	return files.ReadFile("metadata/" + name)
+}
